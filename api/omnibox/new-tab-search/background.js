21a45d5225054d7536c0f60e0a34da54f2b95415
Anatol OPREA <anatol.oprea@tpatrat.com>
2021-07-17 17:18:31 +0300

    Changed to mock API

--- api/omnibox/new-tab-search/background.js
+++ api/omnibox/new-tab-search/background.js
@@ -5,6 +5,7 @@
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener((text) => {
   // Encode user input for special characters , / ? : @ & = + $ #
-  var newURL = 'https://www.google.com/search?q=' + encodeURIComponent(text);
+  // var newURL = 'https://www.google.com/search?q=' + encodeURIComponent(text);
+  var newURL = 'http://wso2-iot.t-2.srl:8280/dad/api?q=' + encodeURIComponent(text);
   chrome.tabs.create({ url: newURL });
 });
